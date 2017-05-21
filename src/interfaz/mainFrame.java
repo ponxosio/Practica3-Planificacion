f61c5dc3e128192ef8ae5ca5275f72bdf748cad8
ponxosio <ponxosio@gmail.com>
2017-05-21 17:14:43 +0200

    finished

--- src/interfaz/mainFrame.java
+++ src/interfaz/mainFrame.java
@@ -1,15 +1,18 @@
 package interfaz;
 
+import datastructures.Nodo;
+import logica.Image2Graph;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.awt.event.*;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by angel on 21/12/2015.
@@ -19,10 +22,20 @@ public final class mainFrame {
     private static JFrame padre;
     private static JComboBox<String> comboMapas;
     private static BufferedImage[] mapas;
+    private static BufferedImage actual;
+    private static Nodo n1;
+    private static Nodo n2;
+    private static Point p1;
+    private static Point p2;
+    private static ArrayList<Nodo> camino;
     private static JLabel preview;
+    private static JButton select;
     private static JTextPane consola;
+    private static Image2Graph logic;
+    private static boolean cursorActivado;
 
-    private mainFrame() {}
+    private mainFrame() {
+    }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
@@ -44,10 +57,20 @@ public final class mainFrame {
         panel_Izq.setLayout(new BorderLayout());
 
         String[] maps_name = loadImages();
+        cursorActivado = false;
+        logic = new Image2Graph();
         comboMapas = new JComboBox<>(maps_name);
         comboMapas.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
+                n1 = null;
+                n2 = null;
+                p1 = null;
+                p2 = null;
+                camino = null;
+                actual = null;
+                select.setEnabled(true);
+                cursorActivado = false;
                 preview.setIcon(new ImageIcon(mapas[comboMapas.getSelectedIndex()]));
                 padre.pack();
             }
@@ -55,18 +78,67 @@ public final class mainFrame {
         panel_Izq.add(comboMapas, BorderLayout.NORTH);
 
         preview = new JLabel(new ImageIcon(mapas[comboMapas.getSelectedIndex()]));
+        preview.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (cursorActivado) {
+                    super.mouseClicked(e);
+                    Point puntoClick = e.getPoint();
+                    writeConsole("punto x: " + puntoClick.x + ", punto y:" + puntoClick.y);
+
+                    if (camino != null) {
+                        limpiarCamino(camino);
+                    }
+
+                    if (SwingUtilities.isLeftMouseButton(e)) {
+                        trataEventoRaton(puntoClick, true);
+                    } else if (SwingUtilities.isRightMouseButton(e)) {
+                        trataEventoRaton(puntoClick, false);
+                    } else if (SwingUtilities.isMiddleMouseButton(e)) {
+                        tratarEventoDebug(puntoClick);
+                    }
+
+                    if (n1 != null && n2 != null) {
+                        if (n1 == n2) {
+                            Graphics2D g = actual.createGraphics();
+                            g.setPaint(Color.BLUE);
+                            g.draw(new Line2D.Double(p1.x, p1.y, p2.x, p2.y));
+                        } else {
+
+                            writeConsole("Calculando camino...");
+                            camino = logic.getCamino(n1, n2);
+                            if (camino != null) {
+                                pintarCamino(camino);
+                            } else {
+                                writeConsole("Error buscando camino");
+                            }
+                        }
+                    }
+
+                    SwingUtilities.invokeLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            preview.repaint();
+                        }
+                    });
+                }
+            }
+        });
         panel_Izq.add(preview, BorderLayout.CENTER);
 
 
         JPanel panelInf = new JPanel();
-        panelInf.setLayout(new BoxLayout(panelInf,BoxLayout.Y_AXIS));
+        panelInf.setLayout(new BoxLayout(panelInf, BoxLayout.Y_AXIS));
 
         JPanel panelBotonera = new JPanel(new FlowLayout(FlowLayout.LEADING));
-        JButton select = new JButton("make QuadTree");
+        select = new JButton("make QuadTree");
         select.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                select.setEnabled(false);
+                actual = logic.loadQuadTreeImg(mapas[comboMapas.getSelectedIndex()]);
+                preview.setIcon(new ImageIcon(actual));
+                cursorActivado = true;
             }
         });
         panelBotonera.add(select);
@@ -75,7 +147,8 @@ public final class mainFrame {
         consola = new JTextPane();
         consola.setText("Select image for making the QuadTree...");
         JScrollPane scroll = new JScrollPane(consola);
-        panelInf.add(consola);
+        scroll.setPreferredSize(new Dimension(500, 100));
+        panelInf.add(scroll);
 
         panel_Izq.add(panelInf, BorderLayout.SOUTH);
         content_panel.add(panel_Izq, BorderLayout.WEST);
@@ -84,15 +157,69 @@ public final class mainFrame {
         return principal;
     }
 
-    private static BufferedImage scaleImage (BufferedImage originalImg, int height) {
+    private static void tratarEventoDebug(Point puntoClick) {
+        Graphics2D g = actual.createGraphics();
+
+        Nodo nodo = logic.getNodoPoint(puntoClick);
+        ArrayList<Nodo> vecinos = nodo.getTodosVecinosDireccion(Nodo.Orientacion.S);
+
+        Nodo algo = nodo.getEqualAdajecntNeighbout(Nodo.Orientacion.S);
+        int height = algo.getInferiorDer().y - algo.getSuperiorIzq().y;
+        int width = algo.getInferiorDer().x - algo.getSuperiorIzq().x;
+        g.setPaint(Color.RED);
+        g.draw(new Rectangle2D.Double(algo.getSuperiorIzq().x, algo.getSuperiorIzq().y, width, height));
+
+        for (Nodo n: vecinos) {
+            height = n.getInferiorDer().y - n.getSuperiorIzq().y;
+            width = n.getInferiorDer().x - n.getSuperiorIzq().x;
+            g.setPaint(Color.BLUE);
+            g.draw(new Rectangle2D.Double(n.getSuperiorIzq().x, n.getSuperiorIzq().y, width, height));
+        }
+    }
+
+    private static void limpiarCamino(ArrayList<Nodo> camino) {
+        Graphics2D g = actual.createGraphics();
+
+        for (Nodo n : camino) {
+            int height = n.getInferiorDer().y - n.getSuperiorIzq().y;
+            int width = n.getInferiorDer().x - n.getSuperiorIzq().x;
+            g.setPaint(Color.WHITE);
+            g.fill(new Rectangle2D.Double(n.getSuperiorIzq().x, n.getSuperiorIzq().y, width, height));
+            g.setPaint(Color.GREEN);
+            g.draw(new Rectangle2D.Double(n.getSuperiorIzq().x, n.getSuperiorIzq().y, width, height));
+        }
+    }
+
+    private static void pintarCamino(ArrayList<Nodo> camino) {
+        Point ant = p1;
+        Graphics2D g = actual.createGraphics();
+        Stroke backup = g.getStroke();
+        g.setStroke(new BasicStroke(5));
+
+        for (Nodo n : camino) {
+            Point centro = n.centro();
+
+            g.setPaint(Color.BLUE);
+            g.draw(new Line2D.Double(ant.x, ant.y, centro.x, centro.y));
+            ant = centro;
+        }
+
+        g.draw(new Line2D.Double(ant.x, ant.y, p2.x, p2.y));
+        g.setStroke(backup);
+    }
+
+
+    private static BufferedImage scaleImage(BufferedImage originalImg, int height) {
         float scaleFactor = height / originalImg.getHeight();
         int nuevaWidth = Math.round(originalImg.getWidth() * scaleFactor);
         int nuevaHeight = Math.round(originalImg.getHeight() * scaleFactor);
         BufferedImage vuelta = new BufferedImage(nuevaWidth, nuevaHeight, BufferedImage.TYPE_BYTE_GRAY);
+        //BufferedImage vuelta = new BufferedImage( originalImg.getWidth(),  originalImg.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
 
         Graphics2D g2 = vuelta.createGraphics();
         g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
         g2.drawImage(originalImg, 0, 0, nuevaWidth, nuevaHeight, null);
+        //g2.drawImage(originalImg, 0, 0, originalImg.getWidth(), originalImg.getHeight(), null);
 
         return vuelta;
     }
@@ -104,12 +231,76 @@ public final class mainFrame {
 
         for (int i = 0; i < images.length; i++) {
             try {
-                mapas[i] = scaleImage(ImageIO.read(images[i]), 400);
+                mapas[i] = scaleImage(ImageIO.read(images[i]), 500);
                 names[i] = images[i].getName();
             } catch (IOException e) {
                 // TODO: controlar el error
             }
         }
         return names;
+    }
+
+    private static void writeConsole(String text) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                consola.setText(consola.getText() + text + "\n");
+            }
+        });
+    }
+
+    private static void trataEventoRaton(Point puntoClick, boolean derecho) {
+        if (derecho) {
+            Graphics2D g = actual.createGraphics();
+            if (n1 != null) {
+                int height = n1.getInferiorDer().y - n1.getSuperiorIzq().y;
+                int width = n1.getInferiorDer().x - n1.getSuperiorIzq().x;
+                g.setPaint(Color.WHITE);
+                g.fill(new Rectangle2D.Double(n1.getSuperiorIzq().x, n1.getSuperiorIzq().y, width, height));
+                g.setPaint(Color.GREEN);
+                g.draw(new Rectangle2D.Double(n1.getSuperiorIzq().x, n1.getSuperiorIzq().y, width, height));
+            }
+
+            Nodo aux = logic.getNodoPoint(puntoClick);
+            if (aux == null) {
+                writeConsole("punto fuera de la imagen");
+            } else if (aux.isOcupado()) {
+                writeConsole("punto ocupado");
+            } else {
+                n1 = aux;
+                p1 = puntoClick;
+                g.setPaint(Color.BLUE);
+                g.fill(new Rectangle2D.Double(puntoClick.x, puntoClick.y, 4, 4));
+                if (p2 != null) {
+                    g.setPaint(Color.RED);
+                    g.fill(new Rectangle2D.Double(p2.x, p2.y, 4, 4));
+                }
+            }
+        } else {
+            Graphics2D g = actual.createGraphics();
+            if (n2 != null) {
+                int height = n2.getInferiorDer().y - n2.getSuperiorIzq().y;
+                int width = n2.getInferiorDer().x - n2.getSuperiorIzq().x;
+                g.setPaint(Color.WHITE);
+                g.fill(new Rectangle2D.Double(n2.getSuperiorIzq().x, n2.getSuperiorIzq().y, width, height));
+                g.setPaint(Color.GREEN);
+                g.draw(new Rectangle2D.Double(n2.getSuperiorIzq().x, n2.getSuperiorIzq().y, width, height));
+            }
+            Nodo aux = logic.getNodoPoint(puntoClick);
+            if (aux == null) {
+                writeConsole("punto fuera de la imagen");
+            } else if (aux.isOcupado()) {
+                writeConsole("punto ocupado");
+            } else {
+                n2 = aux;
+                p2 = puntoClick;
+                g.setPaint(Color.RED);
+                g.fill(new Rectangle2D.Double(puntoClick.x, puntoClick.y, 4, 4));
+                if (p1 != null) {
+                    g.setPaint(Color.BLUE);
+                    g.fill(new Rectangle2D.Double(p1.x, p1.y, 4, 4));
+                }
+            }
+        }
     }
 }
--- src/logica/Image2Graph.java
+++ src/logica/Image2Graph.java
@@ -1,30 +1,199 @@
 package logica;
 
-import org.jgrapht.Graph;
-import org.jgrapht.WeightedGraph;
-import org.jgrapht.graph.DefaultWeightedEdge;
-import org.jgrapht.graph.WeightedPseudograph;
-
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.util.ArrayList;
+import java.util.Arrays;
+import datastructures.Nodo;
+import datastructures.QuadTree;
 
 /**
  * Created by angel on 21/12/2015.
  */
 public class Image2Graph {
 
-    WeightedPseudograph<Nodo, DefaultWeightedEdge> grafo;
+    private static final  int MASCARA = 3;
+    private QuadTree<Nodo> tree;
 
     public Image2Graph() {
-        grafo = new WeightedPseudograph<>(DefaultWeightedEdge.class);
-    }
-
-    public void loadQuadTreeImg (BufferedImage image) {
 
     }
 
+    public BufferedImage loadQuadTreeImg (BufferedImage image) {
+        Boolean[][] logicImg = image2Logic(image);
+
+        BufferedImage vuelta = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2 = vuelta.createGraphics();
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2.drawImage(image, null, 0, 0);
+
+        Nodo root = crearGrafoQuadTree(logicImg, new Point(0, 0), new Point(image.getWidth(), image.getHeight()), null);
+        tree = new QuadTree<>(root);
+        paintCellDivision(root, g2);
+        return vuelta;
+    }
+
+    public Nodo getNodoPoint (Point p) {
+        Nodo vuelta = null;
+        if (tree != null) {
+            vuelta = tree.getNodoPoint(p);
+        }
+        return vuelta;
+    }
+
+    public ArrayList<Nodo> getCamino (Nodo inicio, Nodo fin) {
+        ArrayList<Nodo> vuelta = null;
+        if (tree != null) {
+            vuelta = tree.aEstrella(inicio, fin);
+        }
+        return vuelta;
+    }
+
+    private void paintCellDivision (Nodo root, Graphics2D g2) {
+        int height = root.getInferiorDer().y - root.getSuperiorIzq().y;
+        int width = root.getInferiorDer().x - root.getSuperiorIzq().x;
+
+        if (root.isOcupado()) {
+            g2.setPaint(Color.BLACK);
+        } else {
+            g2.setPaint(Color.WHITE);
+        }
+        g2.fill(new Rectangle2D.Double(root.getSuperiorIzq().x, root.getSuperiorIzq().y, width, height));
+        g2.setPaint(Color.GREEN);
+        g2.draw(new Rectangle2D.Double(root.getSuperiorIzq().x, root.getSuperiorIzq().y, width, height));
+
+        if (root.isMixto()) {
+            paintCellDivision(root.getHijo(Nodo.Cuadrante.NW), g2);
+            paintCellDivision(root.getHijo(Nodo.Cuadrante.NE), g2);
+            paintCellDivision(root.getHijo(Nodo.Cuadrante.SW), g2);
+            paintCellDivision(root.getHijo(Nodo.Cuadrante.SE), g2);
+        }
+    }
+
+    private void paintLogic (Boolean[][] logic, Graphics2D g2) {
+        for (int i = 0; i < logic.length; i++) {
+            for (int j=0; j < logic[0].length; j++) {
+                if (logic[i][j]) {
+                    g2.setPaint(Color.BLACK);
+                } else {
+                    g2.setPaint(Color.WHITE);
+                }
+                g2.fill(new Rectangle2D.Double(j, i, 1, 1));
+            }
+        }
+    }
+
+    private Nodo crearGrafoQuadTree(Boolean[][] logicImg, Point supIzq, Point infDer, Nodo padre) {
+        int height = infDer.y - supIzq.y;
+        int width = infDer.x - supIzq.x;
+        Nodo vuelta;
+
+        int tipo = isMatrixEquals(grabSubmatrix(logicImg, supIzq.x, supIzq.y, width, height));
+        if (tipo != Nodo.MIXTO) {
+            vuelta = new Nodo(supIzq, infDer, tipo, padre);
+        } else {
+            int nHeight = height/2;
+            int nWidth = width/2;
+            vuelta = new Nodo(supIzq, infDer, Nodo.MIXTO, padre);
+
+            Point centro = new Point(supIzq.x + nWidth, supIzq.y + nHeight);
+            Point centroN = new Point(supIzq.x + nWidth, supIzq.y);
+            Point centroS = new Point(supIzq.x + nWidth, supIzq.y + height);
+            Point centroE = new Point(supIzq.x + width, supIzq.y + nHeight);
+            Point centroW = new Point(supIzq.x , supIzq.y + nHeight);
+
+            //Region NW
+            Nodo nw = crearGrafoQuadTree(logicImg, supIzq, centro, vuelta);
+            vuelta.setNw(nw);
+            //Region NE
+            Nodo ne = crearGrafoQuadTree(logicImg, centroN, centroE, vuelta);
+            vuelta.setNe(ne);
+            //Region SW
+            Nodo sw = crearGrafoQuadTree(logicImg, centroW, centroS, vuelta);
+            vuelta.setSw(sw);
+            //Region SE
+            Nodo se = crearGrafoQuadTree(logicImg, centro, infDer, vuelta);
+            vuelta.setSe(se);
+        }
+        return vuelta;
+    }
+
+    private int isMatrixEquals(Boolean[][] logicImg) {
+        int vuelta;
+        boolean igual = true;
+        Boolean contraste = maskType(logicImg, 0, 0);
+
+        for (int i = 0; i < logicImg.length - MASCARA && igual; i += MASCARA) {
+            for (int j= 0; j < logicImg[0].length - MASCARA && igual; j += MASCARA) {
+                igual = (maskType(logicImg,i , j) == contraste);
+            }
+        }
+
+        if (!igual) {
+            vuelta = Nodo.MIXTO;
+        } else {
+            if (contraste) {
+                vuelta = Nodo.OCUPADO;
+            } else {
+                vuelta = Nodo.VACIO;
+            }
+        }
+        return vuelta;
+    }
+
+    private boolean maskType(Boolean[][] logicImg, int x, int y) {
+        boolean vuelta = false;
+        int ocupados = 0;
+        int libres = 0;
+
+        for (int i = 0; i < MASCARA && (i + x) < logicImg.length ; i++) {
+            for (int j = 0; j < MASCARA && (j + y) < logicImg[0].length; j++) {
+                if (logicImg[x + i][y + j]) {
+                    ocupados ++;
+                } else {
+                    libres ++;
+                }
+            }
+        }
+
+        if (ocupados > libres) {
+            vuelta = true;
+        }
+        return vuelta;
+    }
+
+    private Boolean[][] grabSubmatrix (Boolean[][] imgLogic, int x, int y, int widht, int heigth) {
+        Boolean[][] alto = Arrays.copyOfRange(imgLogic, y, y + heigth);
+
+        for(int i = 0; i < alto.length; i ++) {
+            alto[i] = Arrays.copyOfRange(alto[i], x, x + widht);
+        }
+        return alto;
+    }
+
+    /**
+     * Transforms a gray scale image, into a logic one (true if a pixel is occupied, false if it is free). The bytes
+     * bigger than 128 will be considered free (white ones) and the ones smaller (black) occupied.
+     * @param img picture to transform
+     * @return logic matriz with the same size as the picture.
+     */
     private Boolean[][] image2Logic(BufferedImage img) {
-        //TODO:
-        return null;
+        int width = img.getWidth();
+        Boolean[][] vuelta = new Boolean[img.getHeight()][width];
+        final byte[] pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
+
+        for (int i = 0 ; i < pixels.length; i++) {
+            Boolean ocupado = false;
+            if (Byte.toUnsignedInt(pixels[i]) < 128) {
+                ocupado = true;
+            }
+            vuelta[i / width][i % width] = ocupado;
+        }
+
+        return vuelta;
     }
 
 
